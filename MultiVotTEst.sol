4e1fb8fcd6ff1a39c27656a01c553fc0c9543d89
JaishaunDorer1988 <101620297+JaishaunDorer1988@users.noreply.github.com>
2023-11-21 08:33:59 +0100

    Update MultiVotTEst.sol

--- MultiVotTEst.sol
+++ MultiVotTEst.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MultiOptionVoting {
+    mapping(address => bool) public hasVoted;
+    mapping(uint256 => uint256) public votes;
+
+    uint256 public totalOptions;
+
+    event VoteCasted(address indexed voter, uint256 option);
+
+    modifier validOption(uint256 _option) {
+        require(_option > 0 && _option <= totalOptions, "Invalid option");
+        _;
+    }
+
+    modifier notVoted() {
+        require(!hasVoted[msg.sender], "You have already voted");
+        _;
+    }
+
+    constructor(uint256 _totalOptions) {
+        require(_totalOptions > 0, "Total options must be greater than 0");
+        totalOptions = _totalOptions;
+    }
+
+    
+}
